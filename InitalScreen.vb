9523bbd5c677a6a09c0de0a264f9e692f2288e0e
aleksej25 <azabijakin1@gmail.com>
2022-10-29 11:15:04 -0400

    new additions

--- InitalScreen.vb
+++ InitalScreen.vb
@@ -1,3 +1,11 @@
 ﻿Public Class InitalScreen
+    Private Sub OwnerButton_Click(sender As Object, e As EventArgs) Handles OwnerButton.Click
+        Me.Visible = False
+        ' Now load the owners screen
+    End Sub
 
+    Private Sub RiderButton_Click(sender As Object, e As EventArgs) Handles RiderButton.Click
+        Me.Visible = False
+        ' Now load the users screen
+    End Sub
 End Class
---
+++ OwnerMainScreen.vb
@@ -0,0 +1,3 @@
+﻿Public Class OwnerMainScreen
+
+End Class
