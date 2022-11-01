eb89031df0eb1da9810275c3c6def96bcaa294e1
aleksej25 <azabijakin1@gmail.com>
2022-11-01 19:42:49 -0400

    before group work

--- RiderMainScreen.Designer.vb
+++ RiderMainScreen.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class RiderMainScreen
     Inherits System.Windows.Forms.UserControl
 
     'UserControl overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,33 +20,47 @@ Partial Class RiderMainScreen
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.BackBox = New System.Windows.Forms.PictureBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.BackBox, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
+        'BackBox
+        '
+        Me.BackBox.Image = Global.RentECar.My.Resources.ProjectResources.backArrow
+        Me.BackBox.Location = New System.Drawing.Point(36, 32)
+        Me.BackBox.Name = "BackBox"
+        Me.BackBox.Size = New System.Drawing.Size(40, 40)
+        Me.BackBox.TabIndex = 9
+        Me.BackBox.TabStop = False
+        '
         'PictureBox1
         '
-        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark
         Me.PictureBox1.Image = Global.RentECar.My.Resources.ProjectResources.iphone_blank
         Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(335, 643)
-        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.Size = New System.Drawing.Size(333, 640)
+        Me.PictureBox1.TabIndex = 8
         Me.PictureBox1.TabStop = False
         '
         'RiderMainScreen
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Controls.Add(Me.BackBox)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "RiderMainScreen"
-        Me.Size = New System.Drawing.Size(335, 642)
+        Me.Size = New System.Drawing.Size(337, 639)
+        CType(Me.BackBox, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents BackBox As PictureBox
     Friend WithEvents PictureBox1 As PictureBox
 End Class
