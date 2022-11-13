97ec3b110c56dd2c9366a70d9f57e116dc6b2765
Ahmed Begovic <ahmedbegovic00@gmail.com>
2022-11-13 18:52:26 -0500

    Master update

--- InitalScreen.vb
+++ InitalScreen.vb
@@ -3,12 +3,20 @@
         If UserNameBox.Text = "owner" And PasswordBox.Text = "ownerpassword" Then
             Me.Hide()
             Form1.OwnerMainScreen1.Show()
+            Me.UserNameBox.Text = ""
+            Me.PasswordBox.Text = ""
+            Me.InvalidLoginLabel.Hide()
             Form1.riderAccess = False
         ElseIf UserNameBox.Text = "rider" And PasswordBox.Text = "riderpassword" Then
             Me.Hide()
             Form1.RiderMainScreen1.Show()
             Form1.riderAccess = True
+            Me.UserNameBox.Text = ""
+            Me.PasswordBox.Text = ""
+            Me.InvalidLoginLabel.Hide()
         Else
+            Me.UserNameBox.Text = ""
+            Me.PasswordBox.Text = ""
             Me.InvalidLoginLabel.Show()
         End If
     End Sub
